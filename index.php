fe9bff84d0db7bb801a21b448dcb2a7200a6bba6
Rácz Igor <racz.igor@gmail.com>
2020-06-23 18:06:25 +0200

    minor change

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
             </div>
             <div class="col col-md-6">
                 <div class="d-flex justify-content-md-between">
-                    <a class="d-none d-md-block" style="color: blue;" href="index.html">
+                    <a class="d-none d-md-block" style="color: blue;" href="index.php">
                         <svg class="bi bi-house-door-fill" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor"
                             xmlns="http://www.w3.org/2000/svg">
                             <path
@@ -50,7 +50,7 @@
                             <path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
                         </svg>
                     </a>
-                    <a class="d-none d-md-block text-dark text-decoration-none" href="sites.html">
+                    <a class="d-none d-md-block text-dark text-decoration-none" href="">
                         <svg class="bi bi-flag" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor"
                             xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd"
@@ -59,7 +59,7 @@
                                 d="M3.762 2.558C4.735 1.909 5.348 1.5 6.5 1.5c.653 0 1.139.325 1.495.562l.032.022c.391.26.646.416.973.416.168 0 .356-.042.587-.126a8.89 8.89 0 0 0 .593-.25c.058-.027.117-.053.18-.08.57-.255 1.278-.544 2.14-.544a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-.5.5c-.638 0-1.18.21-1.734.457l-.159.07c-.22.1-.453.205-.678.287A2.719 2.719 0 0 1 9 9.5c-.653 0-1.139-.325-1.495-.562l-.032-.022c-.391-.26-.646-.416-.973-.416-.833 0-1.218.246-2.223.916a.5.5 0 1 1-.515-.858C4.735 7.909 5.348 7.5 6.5 7.5c.653 0 1.139.325 1.495.562l.032.022c.391.26.646.416.973.416.168 0 .356-.042.587-.126.187-.068.376-.153.593-.25.058-.027.117-.053.18-.08.456-.204 1-.43 1.64-.512V2.543c-.433.074-.83.234-1.234.414l-.159.07c-.22.1-.453.205-.678.287A2.719 2.719 0 0 1 9 3.5c-.653 0-1.139-.325-1.495-.562l-.032-.022c-.391-.26-.646-.416-.973-.416-.833 0-1.218.246-2.223.916a.5.5 0 0 1-.554-.832l.04-.026z" />
                         </svg>
                     </a>
-                    <a class="d-none d-md-block text-dark text-decoration-none" href="sites.html">
+                    <a class="d-none d-md-block text-dark text-decoration-none" href="">
                         <svg class="bi bi-view-list" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor"
                             xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd"
